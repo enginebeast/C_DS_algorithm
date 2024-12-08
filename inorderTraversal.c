0af2631acf83adf77125445c188714ccfbf064bb
이정환 <ljh34701352@gmail.com>
2024-12-08 12:25:59 +0900

    초기 커밋.

---
+++ inorderTraversal.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Data structure
+typedef struct tree_node{
+	char data;
+	struct tree_node *left_ptr, *right_ptr;
+}tree_node;
+typedef tree_node* tree_ptr;
+
+
+int main(void) {
+	tree_ptr tree[9];
+
+	return 0;
+}
+
+
+//udf
