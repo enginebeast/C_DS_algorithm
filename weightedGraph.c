86acf0cbed7403f221d77fee090e8a8082fe632c
이정환 <ljh34701352@gmail.com>
2024-12-09 11:43:12 +0900

    Add weight. When print graphf, if there is not edge, print empty.

--- weightedGraph.c
+++ weightedGraph.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Data structure
 #define MAX_VERTICES 1000
 
 typedef struct link_node {
-	int data;
+	int data, weight;
 	struct link_node* link;
 }link_node;
 
@@ -15,7 +16,7 @@ short visited[MAX_VERTICES] = { 0 };
 
 
 //udf
-void insertEdge(int u, int v);
+void insertEdge(int u, int v, int weight);
 void printGraph(int vertexNum);
 
 
@@ -27,11 +28,16 @@ int main(void) {
 	printf("Please input the number of edges: ");
 	scanf("%d", &edgeNum);
 
-	int u, v;
+	int u, v, weight;
 	for (int i = 0; i < edgeNum; i++) {
-		printf("Please input the start vertex and the end vertex: ");
-		scanf("%d %d", &u, &v);
-		insertEdge(u, v);
+		printf("Input %d\n", i + 1);
+		printf("Please input the start vertex: ");
+		scanf("%d", &u);
+		printf("Please input the end vertex: ");
+		scanf("%d", &v);
+		printf("Please input the weight: ");
+		scanf("%d", &weight);
+		insertEdge(u, v, weight);
 	}
 
 	//Print graph
@@ -43,9 +49,11 @@ int main(void) {
 
 
 //udf
-void insertEdge(int u, int v) {
+void insertEdge(int u, int v, int weight) {
 	link_node* new_ptr = (link_node*)malloc(sizeof(link_node));
 	new_ptr->data = v;
+	new_ptr->weight = weight;
+
 	if (graph[u] == NULL) {
 		new_ptr->link = NULL;
 	}
@@ -57,9 +65,18 @@ void insertEdge(int u, int v) {
 
 void printGraph(int vertexNum) {
 	for (int i = 1; i <= vertexNum; i++) {
-		printf("%d : ", i);
-		for (node_ptr w = graph[i]; w; w = w->link)
-			printf("%d ", w->data);
+		printf("%d\n", i);
+
+		int flag = 0;
+		for (node_ptr w = graph[i]; ; w = w->link) {
+			if (w == NULL){
+				if (flag == 0)
+					printf("Empty");
+				break;
+			}
+			printf("(End vertex: %d, Weight: %d) ", w->data, w->weight);
+			flag = 1;
+		}
 		printf("\n");
 	}
 }
