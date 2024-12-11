9e925b2b0db8fae31ae712d6b44748d6ea82a7f9
이정환 <ljh34701352@gmail.com>
2024-12-11 23:10:30 +0900

    Add heap full and heap empty.

--- MaxHeap.c
+++ MaxHeap.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 //Data structure
-#define HEAP_MAX_SIZE 1000
+#define HEAP_MAX_SIZE 5
+#define HEAP_FULL(n) (n == HEAP_MAX_SIZE - 1)
+#define HEAP_EMPTY(n) (!n)
 
 int heap[HEAP_MAX_SIZE] = { 0 }; 
-int tail = 0;
+int size = 0;
 
 
 //udf
@@ -43,7 +45,7 @@ int main(void) {
 
 	int nodeNum = 1, levelEnd = 1;
 	printf("This is the max heap inserted.\n");
-	for (int i = 1; i <= tail; i++) {
+	for (int i = 1; i <= size; i++) {
 		printf("%d ", heap[i]);
 		if (levelEnd == i) {
 			printf("\n");
@@ -58,7 +60,12 @@ int main(void) {
 
 //UDF
 void insertHeap(int num) {
-	int i = ++tail;
+	if (HEAP_FULL(size)) {
+		printf("The heap is full.\n");
+		return;
+	}
+
+	int i = ++size;
 	heap[i] = num;
 	int temp;
 	while (i != 1 && heap[i] > heap[i / 2]) {
@@ -70,12 +77,17 @@ void insertHeap(int num) {
 }
 
 void deleteHeap(void) {
+	if (HEAP_EMPTY(size)) {
+		printf("The heap is empty.\n");
+		return;
+	}
+
 	int parent = 1, child = 2;
-	heap[parent] = heap[tail--];
+	heap[parent] = heap[size--];
 	int temp;
-	while (child <= tail) {
+	while (child <= size) {
 		//Compare siblings
-		if (child < tail && heap[child]<heap[child+1]) 
+		if (child < size && heap[child]<heap[child+1]) 
 			child++;
 		//Compare parent with child
 		if (heap[parent] > heap[child])
